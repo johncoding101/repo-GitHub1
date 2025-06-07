988ed6688d63ea353703d52a818afa13b9936c81
johncoding101 <113309060+johncoding101@users.noreply.github.com>
2025-06-07 16:14:52 -0400

    Add files via upload

---
+++ function1.py
@@ -0,0 +1,8 @@
+# Function print
+def function_print():
+    """_summary_
+    """
+    saludos = "saludo"
+    print(saludos)
+    print("otro saludo")
+    print("no hace el ignore")
---
+++ main.py
@@ -0,0 +1,5 @@
+# main program
+print("Main Program")
+print("optimize main")
+print("we used .gitignore")
+print("no me esta haciendo el ignore")
